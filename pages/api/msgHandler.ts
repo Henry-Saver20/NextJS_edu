241221fbf2e9dda39b1036ab7227dfe4806d8777
August Black <augustblack@gmail.com>
2021-06-24 10:28:09 -0600

    appease ts compiler/linter with void return

--- pages/api/msgHandler.ts
+++ pages/api/msgHandler.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest } from 'next'
 import { NextApiResponseServerIO } from '../types/next'
 
-export default (req: NextApiRequest, res: NextApiResponseServerIO) => {
+export default (req: NextApiRequest, res: NextApiResponseServerIO): void => {
   if (req.method === 'POST') {
     const message = req.body
     res?.socket?.server?.io?.emit('message', message) // Sends messege to all connected users when POST req is made by a user
