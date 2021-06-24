b0dcf51ac61563de96244e0a499748a54f10752e
August Black <augustblack@gmail.com>
2021-06-24 10:40:44 -0600

    add <PrivateChatLine />

--- components/chatLine.tsx
+++ components/chatLine.tsx
@@ -12,4 +12,16 @@ const ChatLine : FC<ChatLineProps> = ({
   m = '<no message>'
 }: ChatLineProps) => (<p className='cLine' key={id}><span>{name}: {m}</span></p>)
 
+type PrivateChatLineProps = ChatLineProps & {
+  from : string
+}
+
+export const PrivateChatLine : FC<PrivateChatLineProps> = ({
+  id = 'wtf',
+  name = 'anon',
+  m = '<no message>',
+  from = '<nobody>'
+}: PrivateChatLineProps) => (<p className='cLine' key={id}><span><em>(from:{from})</em> {name}: {m} </span></p>)
+
+
 export default ChatLine
